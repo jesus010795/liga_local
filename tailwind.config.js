51a1b6c5a51337fe9d764c5502698fd0ad6a2956
jesus <bep010795@gmail.com>
2024-04-10 23:26:25 -0600

    SetUps

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#34a853',
+        secondary:'#353535'
+       },
+    },
+  },
+  plugins: [],
+}
+
